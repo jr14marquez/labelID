177ef5dcd72f555a9366747fb662daa4bba593ef
JR Marquez <Jr14marquez@gmail.com>
2017-09-08 20:10:22 -0400

    init repo

---
+++ client/src/components/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      map: '',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
