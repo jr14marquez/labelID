7bc19395b2bcaf29cab89b4e3a57183becbf1b18
JR Marquez <Jr14marquez@gmail.com>
2017-09-08 22:36:23 -0400

    include nav,banner,map components to app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,47 @@
 <template>
-  <div id="app">
+  <div id="app-view">
+    <banner-app></banner-app>
+    <nav-app></nav-app>
+    <div class="container-fluid">
+      <div class="col-md-8">
+        <map-app></map-app>
+      </div>
+      <div class="col-md-4">
+        <map-app></map-app>
+      </div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import NavApp from './components/Nav';
+import BannerApp from './components/Banner';
+import MapApp from './components/Map';
+
 export default {
   name: 'app',
+  data() {
+    return {
+
+    };
+  },
+  components: {
+    NavApp,
+    BannerApp,
+    MapApp,
+  },
 };
 </script>
 
 <style>
+
+html,body{
+  height:100%;
+  widht:100%;
+  background-color:#2c3339;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,4 +50,12 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#app-view {
+  height:100%;
+  width:100%;
+}
+
+
+
 </style>
