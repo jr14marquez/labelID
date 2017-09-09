dbf11a01b3f1573b2e30f889d6a428f7083d2ad2
JR Marquez <Jr14marquez@gmail.com>
2017-09-08 22:32:17 -0400

    add banner component

---
+++ client/src/components/Banner.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container bannerView">
+  	<p><b> {{ classification }} </b></p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      classification: 'UNCLASSIFIED',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.bannerView{
+	text-align: center;	
+	background-color:yellow;
+	color:black;
+  width:100%;
+  height:20px;
+}
+
+</style>
